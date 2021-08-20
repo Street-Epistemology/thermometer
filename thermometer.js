d7fa2f44674077ba78e09b29e1ee7eb895c0e730
FK <70242832+FKonGithub@users.noreply.github.com>
2021-08-20 12:18:18 +0200

    fixed served file

--- thermometer.js
+++ thermometer.js
@@ -16,7 +16,7 @@ const vip = ["563797322798989330"//FK
 // '.'+url.parse(request.url).pathname
 http.createServer(function (request, response) {
   response.writeHead(200)
-  rs = fs.createReadStream('thermometer.svg').on('error', (e)=>{
+  rs = fs.createReadStream('ws.html').on('error', (e)=>{
   console.log(e.message); response.writeHead(404); response.end()}).pipe(response)
 }).listen(80)
 
